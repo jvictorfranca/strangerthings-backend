9a280211aa32e89b17ec7ba62a21b8b0b8aaf7b7
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2022-01-26 15:03:41 -0300

    adiciona o pm2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -29,6 +29,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log('escutando na porta PORT');
 });
